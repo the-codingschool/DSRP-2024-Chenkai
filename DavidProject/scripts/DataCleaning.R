15eabf26e1b4efe2d42d39550f26a447d412a024
David Zeng <davidzeng988@gmail.com>
2024-07-23 14:40:55 -0400

    Preliminary Dataset Exploration

---
+++ DavidProject/scripts/DataCleaning.R
@@ -0,0 +1,9 @@
+getwd()
+
+## read in dataset
+#fileName <- list.files("data")
+#house_data <- read.csv(paste0("data/",fileName))
+#head(house_data)
+
+library(readr)
+library(data.table) #this library contains the fread() function
