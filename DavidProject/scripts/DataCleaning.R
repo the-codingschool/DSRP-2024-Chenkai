906df5fdea69275699eb9df2cc6a59a1ea4fc2ff
David Zeng <davidzeng988@gmail.com>
2024-07-25 09:40:11 -0400

    Deleted Unnecessary File

--- DavidProject/scripts/DataCleaning.R
+++
@@ -1,9 +0,0 @@
-getwd()
-
-## read in dataset
-#fileName <- list.files("data")
-#house_data <- read.csv(paste0("data/",fileName))
-#head(house_data)
-
-library(readr)
-library(data.table) #this library contains the fread() function
